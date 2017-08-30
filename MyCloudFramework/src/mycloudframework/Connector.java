47e3fa36fb0977dc8ac9b0336ca617e535b66b9a
Kristoffer S. Breitenstein <krb@secomea.com>
2017-08-30 13:36:21 +0200

    Start Structure

---
+++ MyCloudFramework/src/mycloudframework/Connector.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycloudframework;
+
+/**
+ *
+ * @author krb
+ */
+public class Connector {
+    
+    public void connectToAWS(){
+    }
+    
+    public void connectToGC(){
+    }
+}
